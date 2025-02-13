2422da1ba4f162da9734f07c93ebaf2c6528473d
tamiayuastuti <2223617.tami@smkn-2sbg.sch.id>
2025-02-13 09:07:26 +0700

    menambahkan komentar

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -21,9 +21,16 @@ return new class extends Migration
     //Metode up() digunakan dalam migration Laravel untuk menjalankan perubahan pada database, seperti membuat atau mengubah tabel.
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            //Kode ini digunakan dalam migration Laravel untuk membuat tabel failed_jobs di database.
+            //Tabel failed_jobs digunakan untuk menyimpan pekerjaan yang gagal saat diproses dalam queue Laravel.
             $table->string('email')->index();
+            // Kolom email menyimpan email pengguna dan diberi index agar pencarian cepat.
             $table->string('token');
+            //Kolom token menyimpan kode unik untuk reset password atau verifikasi akun.
             $table->timestamp('created_at')->nullable();
+            //Membuat kolom created_at dengan tipe data timestamp (tanggal & waktu).
+            //Menyimpan waktu kapan data dibuat, misalnya kapan pengguna meminta reset password.
+            //->nullable() berarti kolom ini boleh kosong (tidak wajib diisi)
         });
     }
 
--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -5,6 +5,12 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
+//Ini membuat kelas anonim yang langsung mewarisi Migration
+//up() Fungsi ini dijalankan saat migrasi dilakukan (php artisan migrate).
+//Membuat tabel products dengan kolom id, nama, dan harga.
+//down()Fungsi ini dijalankan saat rollback (php artisan migrate:rollback).
+//Menghapus tabel products jika ada.
+
 {
     /**
      * Run the migrations.
@@ -12,10 +18,18 @@ return new class extends Migration
      * @return void
      */
     public function up()
+    //Di Laravel, public function up() digunakan dalam migration untuk membuat atau mengubah tabel di database.
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
+            //Kode ini digunakan dalam migration Laravel untuk membuat tabel failed_jobs di database.
+            //Tabel failed_jobs digunakan untuk menyimpan pekerjaan yang gagal saat diproses dalam queue Laravel.
             $table->id();
+            //Membuat kolom id otomatis di tabel.
+            //Tipe datanya BIGINT (20) UNSIGNED → Angka besar tanpa tanda negatif.
+            //Menjadi PRIMARY KEY → Setiap baris memiliki id unik.
+            //Auto-increment → Nilainya akan bertambah otomatis setiap ada data baru
             $table->string('uuid')->unique();
+            
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
